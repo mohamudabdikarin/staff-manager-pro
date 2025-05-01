179ba0737a651b22c003339c79e6d068f3116248
mohamud24 <maxamuud632@gmail.com>
2025-05-01 10:06:39 +0300

    created Response entity practice

--- src/main/java/com/example/staffsystem_C1220642/GreetingController.java
+++ src/main/java/com/example/staffsystem_C1220642/GreetingController.java
@@ -1,9 +1,14 @@
 package com.example.staffsystem_C1220642;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.Collection;
 
 @RestController
 public class GreetingController {
@@ -17,4 +22,27 @@ public class GreetingController {
     public String greet(@PathVariable String name){
         return "Hi "+name+" welcome to my application feel free to ask help";
     }
+
+//    Part two-Response entity
+    @GetMapping("/success")
+    public ResponseEntity<String> success() {
+        return ResponseEntity.ok()
+                .header("Custom-header", "Success Header")
+                .body("Operation successful");
+    }
+
+    @GetMapping("/not-found")
+    public ResponseEntity<String> notFound() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource not found");
+    }
+
+
+    @PostMapping("/create")
+    public ResponseEntity<String> create(@RequestBody String item){
+        URI location = URI.create("/created-resource/1");
+        return ResponseEntity.created(location).body("resource created successfuly");
+
+    }
+
+
 }
